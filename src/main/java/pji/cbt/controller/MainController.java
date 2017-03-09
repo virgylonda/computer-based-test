637e47dcd3f4673738077bef5a8b5509dc6d46c2
Firdaus <Firdaus@FIRDAUS-LAPTOP>
2017-03-09 11:21:44 +0700

    fix conflict maincontroller

--- src/main/java/pji/cbt/controller/MainController.java
+++ src/main/java/pji/cbt/controller/MainController.java
@@ -1,22 +1,50 @@
 package pji.cbt.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import pji.cbt.iservices.AdminService;
+import pji.cbt.entities.User;
+import pji.cbt.iservices.SoalService;
+import pji.cbt.iservices.UserService;
 
 @Controller
 public class MainController {
 
 	@Autowired
-	private AdminService adminSvc;
+	private UserService userSvc;
     
+	@Autowired
+	private SoalService soalSvc;
 	
-    @RequestMapping(value="/login")
-    public String homeLogin(){
+    @RequestMapping(value="/login",method = RequestMethod.GET)
+    public String homeLogin(Model model){
     	return "login";
     }
     
+//    @RequestMapping(value="/login/proccess",method = RequestMethod.POST)
+//    public String proccessLogin(String username, String password, HttpServletRequest request, Model model){
+//    	try {	
+//    		User user = userSvc.findOneUser(username);
+////        	String roles = user.getRoles();
+//        	if(user.getPassword().equalsIgnoreCase(password) && roles.equals("Admin")){	
+//        		return "redirect: ../../../admin/dashboard";
+//        	} else {
+//        		return "redirect:../login";
+//        	}
+//    	} catch (Exception e) {
+//    		return "redirect:../login";
+//    	}
+//    }
     
+    
+    @RequestMapping(value="/user/soal", method= RequestMethod.GET)
+    public String soalpage(Model modelsoal){
+    	modelsoal.addAttribute("soal",this.soalSvc.findAllSoal());
+    	return"/index_soal";
+    }
 }
