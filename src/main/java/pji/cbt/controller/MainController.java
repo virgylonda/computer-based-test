fc29f1b6251208c9fae215f1c865ab5cdd0bb479
Risky Miftahul Fajri <Risky Miftahul Fajri@DESKTOP-62J170V>
2017-03-13 14:20:50 +0700

    logout, view html link to logout url in main controller

--- src/main/java/pji/cbt/controller/MainController.java
+++ src/main/java/pji/cbt/controller/MainController.java
@@ -1,6 +1,12 @@
 package pji.cbt.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +23,15 @@ public class MainController {
     @RequestMapping(value="/login",method = RequestMethod.GET)
     public String homeLogin(Model model){
     	return "login";
+    }
+    
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){    
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
     }
     
 //    @RequestMapping(value="/login/proccess",method = RequestMethod.POST)
