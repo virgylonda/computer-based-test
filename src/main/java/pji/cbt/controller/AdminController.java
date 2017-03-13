747bd92afea752dd99f4a67fa165248abc364200
Risky Miftahul Fajri <Risky Miftahul Fajri@DESKTOP-62J170V>
2017-03-13 14:04:28 +0700

    clear import admincontroller

--- src/main/java/pji/cbt/controller/AdminController.java
+++ src/main/java/pji/cbt/controller/AdminController.java
@@ -1,10 +1,8 @@
 package pji.cbt.controller;
 
-import java.security.MessageDigest;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.xml.bind.DatatypeConverter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import pji.cbt.entities.Answer;
