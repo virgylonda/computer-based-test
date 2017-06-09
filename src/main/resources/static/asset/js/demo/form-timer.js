f01c387770c083a1dcf0c84b1253b57ee5f2fb22
Edric Laksa Putra <stephanus_edric@yahoo.com>
2017-06-09 16:09:20 +0700

    finish masalah timer

--- src/main/resources/static/asset/js/demo/form-timer.js
+++ src/main/resources/static/asset/js/demo/form-timer.js
@@ -1,15 +1,14 @@
 function startTimer(duration, display) 
 	{
-		
 	    var timer = duration, minutes, seconds;
+	    var counter = 0;
+	    
 	    setInterval(function () {
 	        minutes = parseInt(timer / 60, 10)
 	        seconds = parseInt(timer % 60, 10);
 	        
 	        minutes = minutes < 10 ? "0" + minutes : minutes;
 	        seconds = seconds < 10 ? "0" + seconds : seconds;
-	        
-	        duration--;
 	        
 	        display.textContent = minutes + ":" + seconds;
 	        timer = parseInt(timer);
@@ -19,6 +18,9 @@ function startTimer(duration, display)
 				document.getElementById("timeALL").value = 0;
 				//window.location.href = "http://localhost:9091";
 	        }
+	        else{
+	        	counter++
+	    	    document.getElementById("limitTime").value = duration-counter;
+	        }
 	    }, 1000);
-	    return duration;
 	}
