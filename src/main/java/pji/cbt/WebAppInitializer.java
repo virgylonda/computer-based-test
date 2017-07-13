25ae615d427d1d415568d599b8f1e915040b9d0d
Andreas Capri <capriandreas@gmail.com>
2017-07-13 16:17:37 +0700

    Authentication Rest

--- src/main/java/pji/cbt/WebAppInitializer.java
+++ src/main/java/pji/cbt/WebAppInitializer.java
@@ -5,12 +5,24 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import pji.cbt.config.JwtFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @Configuration
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan("pji.cbt")
 public class WebAppInitializer{
+	
+	@Bean
+	public FilterRegistrationBean jwtFilter() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/answer/*");
+
+		return registrationBean;
+	}
 
     public static void main(String[] args) throws Exception{
         SpringApplication.run(WebAppInitializer.class, args);
--- src/main/java/pji/cbt/rest/controller/AuthenticationRestController.java
+++ src/main/java/pji/cbt/rest/controller/AuthenticationRestController.java
@@ -1,11 +1,59 @@
 package pji.cbt.rest.controller;
 
+import java.util.Date;
+
+import javax.servlet.ServletException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import pji.cbt.entities.User;
+import pji.cbt.services.UserService;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@CrossOrigin(origins = "http://localhost", maxAge = 3600)
 @RestController
 @RequestMapping("/authentication")
 public class AuthenticationRestController {
+	
+	@Autowired
+	private UserService usrService;
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(@RequestBody User login) throws ServletException {
+
+		String jwtToken = "";
+
+		if (login.getUsername() == null || login.getPassword() == null) {
+			throw new ServletException("Please fill in username and password");
+		}
+
+		String username = login.getUsername();
+		String password = login.getPassword();
+
+		User user = usrService.findOneUser(username);
+
+		if (user == null) {
+			throw new ServletException("Username not found.");
+		}
+		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(); 
+
+		if(!encoder.matches(password, user.getPassword())){
+			throw new ServletException("Invalid login. Please check your name and password.");
+		}
+
+		jwtToken = Jwts.builder().setSubject(username).claim("roles", "user").setIssuedAt(new Date())
+				.signWith(SignatureAlgorithm.HS256, "secretkey").compact();
+
+		return jwtToken;
+	}
 
 }
